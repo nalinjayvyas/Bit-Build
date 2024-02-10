55241d601d8d66b84361e9e76452731dff1261d0
nalinjayvyas <124486307+nalinjayvyas@users.noreply.github.com>
2024-02-10 22:18:00 +0530

    Update gallery.js

--- Bit-Build-main/gallery/gallery.js
+++ Bit-Build-main/gallery/gallery.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const galleryContainer = document.getElementById('galleryContainer');
     const imagePaths = [
+      'student2.jpg',
+      'student3.jpg',
+      'student4.jpg',
       'student1.jpg',
-      'student1.jpg',
-      'student1.jpg',
-      'student1.jpg',
+      'student5.jpg',
       // Add more image paths as needed
     ];
   
